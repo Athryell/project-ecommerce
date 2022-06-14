6123ccdd5fb6478c5b371aa438b5b1c4f3a25a5e
Athryell <aleprovvi@yahoo.it>
2022-06-14 18:49:06 +0200

    Debug true

--- commerce/settings.py
+++ commerce/settings.py
@@ -31,6 +31,7 @@ SECRET_KEY = os.environ.get('COMMERCE_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = (os.environ.get('COMMERCE_DEBUG_VALUE') == 'True')
+DEBUG = True
 
 ALLOWED_HOSTS = ['gee-commerce.herokuapp.com']
 
